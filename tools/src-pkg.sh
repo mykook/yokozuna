c283b3f6ee0dab55e3266c8b0f702149ddacba13
Ryan Zezeski <rzezeski@gmail.com>
2013-07-25 15:24:41 -0400

    Integrate Yokozuna with master

--- tools/src-pkg.sh
+++ tools/src-pkg.sh
@@ -31,7 +31,7 @@ RIAK_DIR=riak-yokozuna-$VSN-src
 TGZ=$RIAK_DIR.tar.gz
 
 pushd $WD
-git clone -b yz-merge-1.3.0 git://github.com/basho/riak.git $RIAK_DIR
+git clone -b yz-merge-master git://github.com/basho/riak.git $RIAK_DIR
 pushd $RIAK_DIR
 make deps
 pushd deps/yokozuna
